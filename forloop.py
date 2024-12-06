785806f99c017674d9792c15ab9d1cb742d6c9db
AndresElacion <andreselacioniii@gmail.com>
2024-12-07 07:58:23 +0800

    forloop (TUPLE, SET, LIST

--- forloop.py
+++ forloop.py
@@ -1,10 +1,42 @@
 # used to iterate over a sequest (string, list, tuple, set)
 # repeat a block of code an exact amout of times
 
-import time
+# import time
 
-for i in range(10, 0, -1):
-    print(i)
-    time.sleep(1)
+# for i in range(10, 0, -1):
+#     print(i)
+#     time.sleep(1)
 
-print("HAPPY NEW YEAR!")
+# print("HAPPY NEW YEAR!")
+
+"""  """
+# List [] = mutable, most flexible
+# Tuple () = immutable, faster
+# Set {} = mutable (add/remore), unordered, NO duplicates, best for membership testing
+
+""" LIST """
+# fruits = ["apple", "mango", "orange"]
+# fruits[0] = "pineapple"
+# fruits.append("coconut")
+# fruits.remove("apple")
+# fruits.pop(1)
+# fruits.clear()
+
+""" TUPLE """
+# fruits = ("apple", "mango", "orange")
+
+""" SET """
+fruits = {"apple", "mango", "orange"}
+# fruits.add("pineapple")
+# fruits.remove("apple")
+# fruits.clear()
+
+""" for fruit in fruits:
+    print(fruit, end=" ") """
+
+fruit = input("Enter a fruit to search for: ")
+
+if fruit in fruits:
+    print(f"{fruit} was found.")
+else:
+    print(f"{fruit} was not found")
