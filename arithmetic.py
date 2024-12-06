22ddf0e94fe5fac4903b82066ae2afcbbba44186
AndresElacion <andreselacioniii@gmail.com>
2024-12-07 06:10:19 +0800

    arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,17 @@
+# + addition
+# - subtraction
+# * multiplication
+# / division (returns a fload)
+# // integer division (returns an integer)
+# % remainder
+
+friends = 5
+
+friends += 1
+friends -= 1
+friends *= 10
+friends /= 2
+friends //= 2
+friends %= 5
+
+print(friends)
