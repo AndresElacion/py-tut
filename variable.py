d975348d8f37c538aa923ad16d3b2de138163156
AndresElacion <andreselacioniii@gmail.com>
2024-12-07 06:03:42 +0800

    variables

---
+++ variable.py
@@ -0,0 +1,14 @@
+# A reusable container for a value (string, integer, float, boolean)
+
+full_name = "Andrei Dev"
+age = 28
+gpa = 3.2
+is_student = True
+
+print(f"Hello {full_name}, im {age} years old")
+print(f"My GPA is {gpa}")
+
+if is_student:
+    print(f"You are a student")
+else:
+    print(f"You are not a student")
