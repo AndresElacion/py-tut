f00847fb17b8fd30efd53a739a1f8db0fb98f494
AndresElacion <andreselacioniii@gmail.com>
2024-12-07 07:44:08 +0800

    for loop

---
+++ forloop.py
@@ -0,0 +1,10 @@
+# used to iterate over a sequest (string, list, tuple, set)
+# repeat a block of code an exact amout of times
+
+import time
+
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)
+
+print("HAPPY NEW YEAR!")
